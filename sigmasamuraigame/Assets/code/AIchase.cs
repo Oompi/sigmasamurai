896093e83cc6fc05f2a8e5f09125396f86dd9107
esac <djesac@gmail.com>
2024-11-07 22:05:03 -0600

    Update AIchase.cs

--- sigmasamuraigame/Assets/code/AIchase.cs
+++ sigmasamuraigame/Assets/code/AIchase.cs
@@ -23,7 +23,7 @@ public class chase : MonoBehaviour
 
         if (distance > distanceBetween)
         {
-            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
             transform.rotation = Quaternion.Euler(Vector3.forward * angle);
         }
     }
